487110a71884477814ec9a47142b73e80e7f5f68
WuTao <ccwutao@gmail.com>
2021-04-06 16:12:18 +0800

    添加项目文件。

---
+++ DragWindowWithContent.cpp
@@ -0,0 +1,28 @@
+﻿/**
+    Enable "Show window contents while dragging" option.
+*/
+
+#include <windows.h>
+#include <iostream>
+
+using std::cout;
+
+
+int main() 
+{
+    BOOL enabled = FALSE;
+
+    SystemParametersInfo(SPI_GETDRAGFULLWINDOWS, NULL, &enabled, 0);
+
+
+    if (!enabled)
+    {
+        SystemParametersInfo(SPI_SETDRAGFULLWINDOWS,
+            TRUE,
+            NULL,
+            SPIF_UPDATEINIFILE | SPIF_SENDCHANGE);
+    }
+    cout << "\"Show window contents while dragging\" option status: " << (enabled? "ON": "OFF") << "\n";
+
+    return 0;
+}
